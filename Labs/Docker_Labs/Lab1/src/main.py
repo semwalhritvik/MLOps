63814368f935431ff65b6111a329604d58c0ccd4
Hritvik Semwal <hritviksemwal@hotmail.com>
2026-01-30 15:11:12 -0500

    Completed Docker Lab 1

--- Labs/Docker_Labs/Lab1/src/main.py
+++ Labs/Docker_Labs/Lab1/src/main.py
@@ -1,22 +1,21 @@
-# Import necessary libraries
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import joblib
 
 if __name__ == '__main__':
-    # Load the Iris dataset
-    iris = load_iris()
-    X, y = iris.data, iris.target
+    # Load the Wine dataset
+    wine = load_wine()
+    X, y = wine.data, wine.target
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train a Random Forest classifier
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    # Train a Support Vector Machine classifier
+    model = SVC(kernel='linear', C=1.0, random_state=42)
     model.fit(X_train, y_train)
 
     # Save the model to a file
-    joblib.dump(model, 'iris_model.pkl')
+    joblib.dump(model, 'wine_model.pkl')
     
-    print("The model training was successful")
+    print("The model training was successful!")
